7053ec073c2442739a617540337f8245c6e677f2
danielntmd <162406516+danielntmd@users.noreply.github.com>
2024-05-27 04:06:23 -0700

    Gpu backend trait (#1)
    * initial
    
    * Added: 512-bit vector addition instructor set and test
    
    * Added: Min and Sub Instruction Sets
    
    * Added: Device Structure as Lazy Static Mutex
    
    * Refactor: Removed lazy_static crate, removed wrapper, added extension trait, removed mutex
    
    * Added: Addition, subtraction function and others
    
    * Added: Clone Instruction Set
    
    * Added and Refactored: Mult and more instruction set operations
    
    * Add/Fix: SRLI instrucitio, TODO: permute
    
    * Restructuring and Adding CUDA compiles
    
    * Implemented mul and Refactored/Cleaned up
    
    * Delete: test file

---
+++ crates/prover/src/core/backend/gpu/mod.rs
@@ -0,0 +1,31 @@
+pub mod error;
+pub mod m31;
+
+use std::sync::Arc;
+
+use cudarc::driver::{CudaDevice, CudaSlice};
+// use error::Error;
+use once_cell::sync::Lazy;
+
+use self::m31::LoadPackedBaseField;
+use crate::core::fields::m31::P;
+const VECTOR_SIZE: usize = 16;
+
+// TODO:: cleanup unwraps with error handling?
+// (We can replace lazy statics with unsafe global references)
+static DEVICE: Lazy<Arc<CudaDevice>> = Lazy::new(|| CudaDevice::new(0).unwrap().load());
+static MODULUS: Lazy<CudaSlice<u32>> =
+    Lazy::new(|| DEVICE.htod_copy([P; VECTOR_SIZE].to_vec()).unwrap());
+
+type Device = Arc<CudaDevice>;
+
+trait Load {
+    fn load(self) -> Self;
+}
+
+impl Load for Device {
+    fn load(self) -> Self {
+        LoadPackedBaseField::load(&self);
+        self
+    }
+}
